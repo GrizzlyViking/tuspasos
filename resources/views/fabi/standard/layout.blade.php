0a69ff1b24760123ebdd8f61e68b8507811967d5
Sebastian <sebastian@edelmann.co.uk>
2015-05-08 11:34:23 +0100

    adjusted size of logo image

--- resources/views/fabi/standard/layout.blade.php
+++ resources/views/fabi/standard/layout.blade.php
@@ -153,7 +153,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a href="{!! route('home') !!}">
-                    {!! HTML::image('images/Logo_web_background.jpeg', 'tuspasos logo', ['style' => 'margin-top: 5px; margin-bottom: -10px;']) !!}
+                    {!! HTML::image('images/Logo_web_background_large.jpeg', 'tuspasos logo', ['style' => 'width: 278px; height: 96px; margin-top: 5px; margin-bottom: -10px;']) !!}
                 </a>
                 <div class="hidden-xs" style="float: right;margin-top: 25px;">
                         tel: 07514 024 008<br>
